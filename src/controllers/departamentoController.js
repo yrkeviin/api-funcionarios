e6b34724ca3f16728e3cd188740be09695069531
yrkeviin <kevineziquiellima160@gmail.com>
2025-05-06 09:57:03 -0300

    Feat: Ajust photo create departamento!

--- src/controllers/departamentoController.js
+++ src/controllers/departamentoController.js
@@ -23,7 +23,9 @@ const getDepartamento = async (req, res) => {
 
 const createDepartamento = async (req, res) => {
     try {
-        const newDepartamento = await departamentoModel.createDepartamento(req.body.name);
+        const { name } = req.body;
+        const photo = req.file ? req.file.filename : null;
+        const newDepartamento = await departamentoModel.createDepartamento(name, photo);;
         res.status(201).json(newDepartamento);
     } catch (error) {
         res.status(500).json({ message: "Erro ao criar departamento!" });
--- src/routes/departamentoRoutes.js
+++ src/routes/departamentoRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const departamentoController = require("../controllers/departamentoController");
+const upload = require("../config/upload");
+const apiKeyMiddleware = require("../config/apiKey"); // 🔐
+
+router.use(apiKeyMiddleware); // 🔒 Aplica para todas as rotas abaixo
 
 router.get("/", departamentoController.getAllDepartamentos);
 router.get("/:id", departamentoController.getDepartamento);
-router.post("/", departamentoController.createDepartamento);
+router.post("/", upload.single("photo"), departamentoController.createDepartamento);
 router.put("/:id", departamentoController.updateDepartamento);
 router.delete("/:id", departamentoController.deleteDepartamento);
 
